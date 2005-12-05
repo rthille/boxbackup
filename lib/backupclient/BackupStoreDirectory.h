be1d7519e4868bdd2f0bf2dbf59374977def4870
Chris Wilson <chris+github@qwirx.com>
2005-12-05 16:37:29 +0000

    * BackupStoreDirectory.h - Fixed spacing to match 0.08

--- lib/backupclient/BackupStoreDirectory.h
+++ lib/backupclient/BackupStoreDirectory.h
@@ -181,7 +181,7 @@ public:
 			// Return entry, and increment
 			return (*(i++));
 		}
-
+	
 		// WARNING: This function is really very inefficient.
 		// Only use when you want to look up ONE filename, not in a loop looking up lots.
 		// In a looping situation, cache the decrypted filenames in another memory structure.
