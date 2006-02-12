c3c1bc58224846d46ad2027348a5267ddd5a232a
Chris Wilson <chris+github@qwirx.com>
2006-02-12 15:07:25 +0000

    * WinNamedPipeStream.cpp - Always call Close() on destruct, as long as we have a socket handle to close

--- lib/win32/WinNamedPipeStream.cpp
+++ lib/win32/WinNamedPipeStream.cpp
@@ -53,7 +53,7 @@ WinNamedPipeStream::WinNamedPipeStream()
 // --------------------------------------------------------------------------
 WinNamedPipeStream::~WinNamedPipeStream()
 {
-	if (mSocketHandle != NULL && IsConnected())
+	if (mSocketHandle != NULL)
 	{
 		Close();
 	}
