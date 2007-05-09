4274319939573fea32324cf45bffe001d7d5baee
Chris Wilson <chris+github@qwirx.com>
2007-05-09 10:22:14 +0000

    Spacing.

--- test/bbackupd/testbbackupd.cpp
+++ test/bbackupd/testbbackupd.cpp
@@ -1889,6 +1889,7 @@ int test_bbackupd()
 		TEST_THAT(!TestFileExists("testfiles/TestDir1/x1/dsfdsfs98.fd"));
 		TEST_THAT(::rename("testfiles/TestDir1/df9834.dsf", 
 			"testfiles/TestDir1/x1/dsfdsfs98.fd") == 0);
+		
 		wait_for_backup_operation();
 		compareReturnValue = ::system(BBACKUPQUERY " -q "
 			"-c testfiles/bbackupd.conf "
@@ -2471,6 +2472,7 @@ int test_bbackupd()
 		// timestamps still get added
 		printf("\n==== Create a file with timestamp way ahead "
 			"in the future\n");
+
 		// Time critical, so sync
 		wait_for_sync_start();
 
@@ -2642,6 +2644,7 @@ int test_bbackupd()
 		if (!ServerIsAlive(bbstored_pid)) return 1;
 
 		printf("\n==== Check restore deleted files\n");
+
 		{
 			SocketStreamTLS conn;
 			conn.Open(context, Socket::TypeINET, "localhost", 
