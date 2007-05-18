8df5096ae55951d31738e01a81138b72fd7e133e
Chris Wilson <chris+github@qwirx.com>
2007-05-18 20:13:55 +0000

    Undo bad merge of [1658] and [1669]

--- bin/bbackupd/BackupDaemon.cpp
+++ bin/bbackupd/BackupDaemon.cpp
@@ -2558,7 +2558,7 @@ BackupDaemon::CommandSocketInfo::~CommandSocketInfo()
 
 static const int STOREOBJECTINFO_MAGIC_ID_VALUE = 0x7777525F;
 static const std::string STOREOBJECTINFO_MAGIC_ID_STRING = "BBACKUPD-STATE";
-static const int STOREOBJECTINFO_VERSION = 2;
+static const int STOREOBJECTINFO_VERSION = 1;
 
 bool BackupDaemon::SerializeStoreObjectInfo(int64_t aClientStoreMarker, box_time_t theLastSyncTime, box_time_t theNextSyncTime) const
 {
@@ -2618,9 +2618,8 @@ bool BackupDaemon::SerializeStoreObjectInfo(int64_t aClientStoreMarker, box_time
 		//
 		//
 		aFile.Close();
-		BOX_INFO("Saved store object info file: " <<
-			StoreObjectInfoFile << ", version " <<
-			STOREOBJECTINFO_VERSION);
+		BOX_INFO("Saved store object info file: "
+			<< StoreObjectInfoFile);
 	}
 	catch(std::exception &e)
 	{
@@ -2774,46 +2773,6 @@ bool BackupDaemon::DeserializeStoreObjectInfo(int64_t & aClientStoreMarker, box_
 
 			mIDMapMounts.push_back(strItem);
 		}
-
-		//
-		//
-		//
-		iCount = 0;
-		anArchive.Read(iCount);
-
-		for(int v = 0; v < iCount; v++)
-		{
-			int64_t anId;
-			anArchive.Read(anId);
-
-			std::string aName;
-			anArchive.Read(aName);
-
-			mUnusedRootDirEntries.push_back(std::pair<int64_t, std::string>(anId, aName));
-		}
-
-		if (iCount > 0)
-			anArchive.Read(mDeleteUnusedRootDirEntriesAfter);
-
-		//
-		//
-		//
-		iCount = 0;
-		anArchive.Read(iCount);
-
-		for(int v = 0; v < iCount; v++)
-		{
-			int64_t anId;
-			anArchive.Read(anId);
-
-			std::string aName;
-			anArchive.Read(aName);
-
-			mUnusedRootDirEntries.push_back(std::pair<int64_t, std::string>(anId, aName));
-		}
-
-		if (iCount > 0)
-			anArchive.Read(mDeleteUnusedRootDirEntriesAfter);
 
 		//
 		//
