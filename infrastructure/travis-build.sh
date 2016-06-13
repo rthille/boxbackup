93520859241c7eab1035c5dfbef3f6918bdb0119
Chris Wilson <chris+github@qwirx.com>
2016-06-13 23:15:06 +0100

    Enable Travis to build with CMake.

---
+++ infrastructure/travis-build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -x
+
+ccache -s
+
+if [ "$BUILD" = 'cmake' ]; then
+	cd infrastructure/cmake/build
+	cmake --version
+	cmake ..
+	ctest
+else
+	./bootstrap
+	./configure CC="ccache $CC" CXX="ccache $CXX"
+	grep CXX config.status
+	make
+	./runtest.pl ALL $TEST_TARGET
+fi
+
+ccache -s
