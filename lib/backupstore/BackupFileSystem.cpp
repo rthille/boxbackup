dcbf4f229a2bda699a04e85bb3758f9b73afb05f
Chris Wilson <chris+github@qwirx.com>
2016-11-27 14:54:00 +0000

    Fix confusion about URIs and paths in S3 filesystem.
    Make it clear (with parameter names and comments) which functions take paths
    (relative to the configured base path), which functions take URIs (complete
    paths) and which functions take URLs (hostname, port and URI).
    
    Put all objects (files and directories) in the same "directory" on S3 so that
    we can list them all together and easily identify the type of each object (from
    its extension). Previously they were separated into "directories" (delimited
    prefixes) called "files" and "dirs" (unlike BackupStore filesystems).

--- lib/backupstore/BackupFileSystem.cpp
+++ lib/backupstore/BackupFileSystem.cpp
@@ -17,14 +17,85 @@
 #include "BackupStoreDirectory.h"
 #include "BackupFileSystem.h"
 #include "CollectInBufferStream.h"
+#include "Configuration.h"
 #include "S3Client.h"
+#include "StoreStructure.h"
 
 #include "MemLeakFindOn.h"
 
-std::string S3BackupFileSystem::GetDirectoryURI(int64_t ObjectID)
+S3BackupFileSystem::S3BackupFileSystem(const Configuration& config,
+	const std::string& BasePath, S3Client& rClient)
+: mrConfig(config),
+  mBasePath(BasePath),
+  mrClient(rClient)
+{ }
+
+std::string S3BackupFileSystem::GetObjectURL(const std::string& ObjectURI) const
 {
+	const Configuration s3config = mrConfig.GetSubConfiguration("S3Store");
+	return std::string("http://") + s3config.GetKeyValue("HostName") + ":" +
+		s3config.GetKeyValue("Port") + ObjectURI;
+}
+
+// --------------------------------------------------------------------------
+//
+// Function
+//		Name:    S3BackupFileSystem::GetObjectURI(int64_t ObjectID,
+//			 int Type)
+//		Purpose: Builds the object filename for a given object,
+//			 including mBasePath. Very similar to
+//			 StoreStructure::MakeObjectFilename(), but files and
+//			 directories have different extensions, and the
+//			 filename is the full object ID, not just the lower
+//			 STORE_ID_SEGMENT_LENGTH bits.
+//		Created: 2016/03/21
+//
+// --------------------------------------------------------------------------
+std::string S3BackupFileSystem::GetObjectURI(int64_t ObjectID, int Type)
+{
+	const static char *hex = "0123456789abcdef";
 	std::ostringstream out;
-	out << mBasePath << "dirs/" << BOX_FORMAT_OBJECTID(ObjectID) << ".dir";
+	ASSERT(mBasePath.size() > 0 && mBasePath[0] == '/' &&
+		mBasePath[mBasePath.size() - 1] == '/');
+	out << mBasePath;
+
+	// Get the id value from the stored object ID into an unsigned int64_t, so that
+	// we can do bitwise operations on it safely.
+	uint64_t id = (uint64_t)ObjectID;
+
+	// Shift off the bits which make up the leafname
+	id >>= STORE_ID_SEGMENT_LENGTH;
+
+	// build pathname
+	while(id != 0)
+	{
+		// assumes that the segments are no bigger than 8 bits
+		int v = id & STORE_ID_SEGMENT_MASK;
+		out << hex[(v & 0xf0) >> 4];
+		out << hex[v & 0xf];
+		out << "/";
+
+		// shift the bits we used off the pathname
+		id >>= STORE_ID_SEGMENT_LENGTH;
+	}
+
+	// append the filename
+	out << BOX_FORMAT_OBJECTID(ObjectID);
+	if(Type == ObjectExists_File)
+	{
+		out << ".file";
+	}
+	else if(Type == ObjectExists_Dir)
+	{
+		out << ".dir";
+	}
+	else
+	{
+		THROW_EXCEPTION_MESSAGE(CommonException, Internal,
+			"Unknown file type for object " << BOX_FORMAT_OBJECTID(ObjectID) <<
+			": " << Type);
+	}
+
 	return out.str();
 }
 
--- lib/backupstore/BackupFileSystem.h
+++ lib/backupstore/BackupFileSystem.h
@@ -15,6 +15,7 @@
 
 #include "HTTPResponse.h"
 #include "S3Client.h"
+#include "Utils.h" // for ObjectExists_File and ObjectExists_Dir
 
 class BackupStoreDirectory;
 class BackupStoreInfo;
@@ -39,17 +40,44 @@ class IOStream;
 class S3BackupFileSystem
 {
 private:
+	const Configuration& mrConfig;
 	std::string mBasePath;
 	S3Client& mrClient;
+
 public:
 	S3BackupFileSystem(const Configuration& config, const std::string& BasePath,
-		S3Client& rClient)
-	: mBasePath(BasePath),
-	  mrClient(rClient)
-	{ }
-	std::string GetDirectoryURI(int64_t ObjectID);
+		S3Client& rClient);
+
+	// These are public to help with writing tests ONLY:
+	// The returned URI should start with mBasePath.
+	std::string GetMetadataURI(const std::string& MetadataFilename) const
+	{
+		return mBasePath + MetadataFilename;
+	}
+	// The returned URI should start with mBasePath.
+	std::string GetDirectoryURI(int64_t ObjectID)
+	{
+		return GetObjectURI(ObjectID, ObjectExists_Dir);
+	}
+	// The returned URI should start with mBasePath.
+	std::string GetFileURI(int64_t ObjectID)
+	{
+		return GetObjectURI(ObjectID, ObjectExists_File);
+	}
+
 	std::auto_ptr<HTTPResponse> GetDirectory(BackupStoreDirectory& rDir);
 	int PutDirectory(BackupStoreDirectory& rDir);
+
+private:
+	// GetObjectURL() returns the complete URL for an object at the given
+	// path, by adding the hostname, port and the object's URI (which can
+	// be retrieved from GetMetadataURI or GetObjectURI). This is only used
+	// for error messages, since S3Client doesn't take URLs.
+	std::string GetObjectURL(const std::string& ObjectURI) const;
+
+	// GetObjectURI() is a private interface which converts an object ID
+	// and type into a URI, which starts with mBasePath:
+	std::string GetObjectURI(int64_t ObjectID, int Type);
 };
 
 #endif // BACKUPFILESYSTEM__H
